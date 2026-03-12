3315049774fcaef50323c13a541ac6b9498e8942
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-03-12 20:47:51 +0200

    feat: add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { ProductPage } from './components/product-page/product-page';
+import { ContactPage } from './components/contact-page/contact-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomePage, title: 'Coffee | Home' },
+  { path: 'products', component: ProductPage, title: 'Coffee | Products' },
+  { path: 'contact', component: ContactPage, title: 'Coffee | Contact' },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Navbar } from './components/navbar/navbar';
-import { Carousel } from './components/carousel/carousel';
-import { ProductCard } from './components/product-card/product-card';
+import { RouterOutlet } from '@angular/router';
+
 import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Carousel, ProductCard, Footer],
+  imports: [Navbar, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
