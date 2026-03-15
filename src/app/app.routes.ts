e18cc7ab6b9389d1188ead82722061b9c2f9d777
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-03-15 12:31:46 +0200

    feat: add 404 not found page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomePage } from './components/home-page/home-page';
 import { ProductPage } from './components/product-page/product-page';
 import { ContactPage } from './components/contact-page/contact-page';
+import { NotfoundPage } from './components/notfound-page/notfound-page';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePage, title: 'Coffee | Home' },
   { path: 'products', component: ProductPage, title: 'Coffee | Products' },
   { path: 'contact', component: ContactPage, title: 'Coffee | Contact' },
+  { path: '**', component: NotfoundPage, title: 'Coffee | Not Found' },
 ];
---
+++ src/app/components/notfound-page/notfound-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-notfound-page',
+  imports: [RouterLink],
+  templateUrl: './notfound-page.html',
+  styleUrl: './notfound-page.css',
+})
+export class NotfoundPage {}
